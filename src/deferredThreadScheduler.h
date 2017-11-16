a12f7b1df93b981f810ea1e67735d39096299447
Massimo Marino <massimo.marino@ieee.org>
2017-11-16 17:58:22 +0100

    refactored; implemented new tests; implemented factory

--- src/deferredThreadScheduler.h
+++ src/deferredThreadScheduler.h
@@ -14,6 +14,7 @@
 #include <atomic>
 #include <condition_variable>
 #include <chrono>
+#include <unistd.h>
 ////////////////////////////////////////////////////////////////////////////////
 namespace deferredThreadScheduler
 {
@@ -28,31 +29,20 @@ public:
 
   // we don't want these objects allocated on the heap
   void* operator new(std::size_t) = delete;
+  void* operator new[](std::size_t) = delete;
+  deferredThreadSchedulerBase() = delete;
 
-  deferredThreadSchedulerBase(const std::string& threadName)
+  // default copy ctor
+  deferredThreadSchedulerBase(const deferredThreadSchedulerBase& rhs) = default;
+  deferredThreadSchedulerBase& operator=(const deferredThreadSchedulerBase& rhs) = default;
+
+  explicit
+  deferredThreadSchedulerBase(const std::string& threadName) noexcept
   :
   threadName_ (threadName)
   {}
 
   virtual ~deferredThreadSchedulerBase();
-
-  void
-  cancelThread() const noexcept
-  {
-    {
-      std::unique_lock<std::mutex> lk(cv_m_);
-      if ( (threadState::NotValid == getThreadState_()) ||
-           (threadState::Scheduled == getThreadState_()) )
-      {
-        setThreadState(threadState::Cancelled);
-      }
-      else
-      {
-        return;
-      }
-    }
-    cv_.notify_one();
-  }
 
   const
   std::string&
@@ -61,6 +51,20 @@ public:
     return threadName_;
   }
 
+  void
+  cancelThread() const noexcept
+  {
+    {
+      std::unique_lock<std::mutex> lk(cv_m_);
+      if ( (threadState::NotValid != getThreadState_()) &&
+           (threadState::Scheduled != getThreadState_()) )
+      {
+        return;
+      }
+      setThreadState(threadState::Cancelled);
+    }
+    cv_.notify_one();
+  }
   constexpr
   auto
   wait() const noexcept
@@ -75,15 +79,24 @@ public:
     return static_cast<int>(threadState_);
   }
 
+  const
+  std::thread::id
+  getThreadId() const noexcept
+  {
+    return threadId_;
+  }
+
  protected:
-  mutable threadState threadState_ {threadState::NotValid};
+  std::string threadName_ {};
+
   // condition variable for thread cancellation notification
-  mutable std::condition_variable cv_;
+  mutable std::condition_variable cv_ {};
   // mutex associated to the condition variable
-  mutable std::mutex cv_m_;
-  const std::string threadName_ {};
-  mutable std::future<int> threadFuture_ {};
-  std::function<int()> f_ {};
+  mutable std::mutex cv_m_ {};
+
+  mutable threadState threadState_ {threadState::NotValid};
+  mutable std::thread::id threadId_ {};
+  mutable std::shared_future<int> threadFuture_ {};
 
   void
   setThreadState(const threadState& threadState) const noexcept
@@ -98,11 +111,17 @@ public:
     return threadState_;
   }
 
-  constexpr
-  std::future<int>&
+  const
+  std::shared_future<int>&
   getThreadFuture() const noexcept
   {
     return threadFuture_;
+  }
+
+  void
+  setThreadId() const noexcept
+  {
+    threadId_ = std::this_thread::get_id();
   }
 };  // class deferredThreadSchedulerBase
 
@@ -111,39 +130,64 @@ using defaultFun = std::function<void()>;
 template <typename F = defaultFun>
 class deferredThreadScheduler final : public deferredThreadSchedulerBase
 {
+ private:
+  mutable std::function<int(const std::chrono::seconds)> f_ {};
+
  public:
   // we don't want these objects allocated on the heap
   void* operator new(std::size_t) = delete;
+  void* operator new[](std::size_t) = delete;
   deferredThreadScheduler() = delete;
+  // default copy ctor
+  deferredThreadScheduler(const deferredThreadScheduler& rhs) = default;
+  deferredThreadScheduler& operator=(const deferredThreadScheduler& rhs) = default;
+
   ~deferredThreadScheduler()
   {}
-
+  
   explicit
-  deferredThreadScheduler(const std::string& threadName,
-                          const F& f,
-                          const std::chrono::seconds& deferredTimeSeconds) noexcept
+  deferredThreadScheduler(const std::string& threadName) noexcept
   :
   deferredThreadSchedulerBase(threadName)
+  {}
+
+  void
+  registerThread(const F& f) const noexcept
   {
-    // create the lambda
-    // NOTE: deferredTimeSeconds MUST be captured by value, NOT by reference
-    f_ = [this, &f, deferredTimeSeconds]() noexcept -> int
+    // create the closure
+    // NOTES: 
+    //   - deferredTimeSeconds MUST be captured by value, NOT by reference
+    //     Reason: testing has shown that if captured by reference the call to wait_for()
+    //     behaves weirdly (the timer seems messed up)
+    //   - f MUST be captured by value; if passed by reference the same lambda
+    //     will be defined in all object instances created
+    f_ = [this, f](const std::chrono::seconds deferredTimeSeconds) noexcept(false) -> int
     {
+      setThreadId();
+
       std::unique_lock<std::mutex> lk(cv_m_);
       // wait on the condition variable until timeout or notification of cancellation
       std::cv_status r = cv_.wait_for(lk, deferredTimeSeconds);
       if ( std::cv_status::timeout == r )
       {
-        // run thread function
-        setThreadState(threadState::Running),
-        f(),
-        setThreadState(threadState::Run);
+        if ( threadState::Scheduled == getThreadState_() )
+        {
+          // run thread function
+          setThreadState(threadState::Running),
+          f(),
+          setThreadState(threadState::Run);
+        }
       }
       return getThreadState();
-    };
-    // run the lambda async
-    threadFuture_ = std::async(std::launch::async, f_);
-    setThreadState(threadState::Scheduled);
+    };    
+  }
+
+  void
+  runIn(const std::chrono::seconds deferredTimeSeconds) const noexcept
+  {
+    // run the closure async
+    threadFuture_ = std::async(std::launch::async, f_, deferredTimeSeconds);
+    setThreadState(threadState::Scheduled);    
   }
 };  // class deferredThreadScheduler
 
@@ -151,7 +195,7 @@ class deferredThreadScheduler final : public deferredThreadSchedulerBase
 // factory
 
 template <typename F>
-using deferredThreadSchedulerPtr = std::unique_ptr<deferredThreadScheduler<F>>;
+using deferredThreadSchedulerUniquePtr = std::unique_ptr<deferredThreadScheduler<F>>;
 
 // create an object of type T and return a std::unique_ptr to it
 template <typename T, typename... Args>
@@ -162,12 +206,28 @@ createUniquePtr(Args&&... args) -> std::unique_ptr<T>
 }
 
 template <typename F>
-deferredThreadSchedulerPtr<F>
-makeDeferredThreadScheduler(const std::string& threadName,
-                            const F& f,
-                            const std::chrono::seconds& deferredTimeSeconds) noexcept
+deferredThreadSchedulerUniquePtr<F>
+makeUniqueDeferredThreadScheduler(const std::string& threadName) noexcept
 {
-  return createUniquePtr<deferredThreadScheduler<F>>(threadName, f, deferredTimeSeconds);
+  return createUniquePtr<deferredThreadScheduler<F>>(threadName);
+}
+////////////////////////////////////////////////////////////////////////////////
+template <typename F>
+using deferredThreadSchedulerSharedPtr = std::shared_ptr<deferredThreadScheduler<F>>;
+
+// create an object of type T and return a std::shared_ptr to it
+template <typename T, typename... Args>
+auto
+createSharedPtr(Args&&... args) -> std::shared_ptr<T>
+{
+  return std::make_shared<T>(args...);
+}
+
+template <typename F>
+deferredThreadSchedulerSharedPtr<F>
+makeSharedDeferredThreadScheduler(const std::string& threadName) noexcept
+{
+  return createSharedPtr<deferredThreadScheduler<F>>(threadName);
 }
 }  // namespace deferredThreadScheduler
 #endif /* DEFERRED_THREAD_SCHEDULER_H */
--- src/unitTests/unitTests.cpp
+++ src/unitTests/unitTests.cpp
@@ -7,6 +7,7 @@
 
 #include "../deferredThreadScheduler.h"
 #include "concurrentLogging.h"
+#include <vector>
 #include <gtest/gtest.h>
 #include <gmock/gmock.h>
 
@@ -28,23 +29,37 @@ TEST(deferredThreadScheduler, test_1)
   using defaultFun = std::function<void()>;
   defaultFun voidFoo = []() -> void
   {
-    std::cout << "[ " << __func__ << " ] "
-              << "voidFoo() running................................."
-              << std::endl;
+    utilities::pclog{} << "[ " << __func__ << " ] "
+                       << "voidFoo() running................................."
+                       << std::endl;
   };
 
-  deferredThreadScheduler<defaultFun> dts {"voidFoo", voidFoo, 5s};
+  auto deferredTime = 5s;
+  deferredThreadScheduler<defaultFun> dts {"voidFoo"};
+  dts.registerThread(voidFoo);
 
   ASSERT_EQ(dts.getThreadState(),
-          static_cast<int>(deferredThreadSchedulerBase::threadState::Scheduled));
+            static_cast<int>(deferredThreadSchedulerBase::threadState::NotValid));
+
+  dts.runIn(deferredTime);
+
+  ASSERT_EQ(dts.getThreadState(),
+            static_cast<int>(deferredThreadSchedulerBase::threadState::Scheduled));
+
+  while ( static_cast<int>(deferredThreadSchedulerBase::threadState::Scheduled) == dts.getThreadState() )
+  {
+    usleep(1'000);
+  }
 
   std::cout << "[ " << __func__ << " ] "
-             << "Thread name: "
+            << "Thread name: "
             << dts.getThreadName() << std::endl;
   std::cout << "[ " << __func__ << " ] "
             << "waiting for thread "
             << dts.getThreadName()
-            << " to run..." << std::endl;
+            << " [ id 0x"
+            << std::hex << dts.getThreadId() << std::dec
+            << " ] to run..." << std::endl;
   dts.wait();
 
   ASSERT_EQ(dts.getThreadState(),
@@ -60,25 +75,39 @@ TEST(deferredThreadScheduler, test_2)
   defaultFun voidFoo = [&threadArg] () -> void
   {
     threadArg += "from voidFoo()!!!";
-    std::cout << "[ " << __func__ << " ] "
-              << "voidFoo() running: "
-              << threadArg
-              << "................................."
-              << std::endl;
+    utilities::pclog{} << "[ " << __func__ << " ] "
+                       << "voidFoo() running: "
+                       << threadArg
+                       << "................................."
+                       << std::endl;
   };
 
-  deferredThreadScheduler<defaultFun> dts {"voidFoo", voidFoo, 2s};
+  auto deferredTime = 3s;
+  deferredThreadScheduler<defaultFun> dts {"voidFoo"};
+  dts.registerThread(voidFoo);
 
   ASSERT_EQ(dts.getThreadState(),
-        static_cast<int>(deferredThreadSchedulerBase::threadState::Scheduled));
+            static_cast<int>(deferredThreadSchedulerBase::threadState::NotValid));
+
+  dts.runIn(deferredTime);
+
+  ASSERT_EQ(dts.getThreadState(),
+            static_cast<int>(deferredThreadSchedulerBase::threadState::Scheduled));
+
+  while ( static_cast<int>(deferredThreadSchedulerBase::threadState::Scheduled) == dts.getThreadState() )
+  {
+    usleep(1'000);
+  }
 
   std::cout << "[ " << __func__ << " ] "
-             << "Thread name: "
+            << "Thread name: "
             << dts.getThreadName() << std::endl;
   std::cout << "[ " << __func__ << " ] "
             << "waiting for thread "
             << dts.getThreadName()
-            << " to run..." << std::endl;
+            << " [ id 0x"
+            << std::hex << dts.getThreadId() << std::dec
+            << " ] to run..." << std::endl;
   dts.wait();
 
   ASSERT_EQ(dts.getThreadState(),
@@ -95,25 +124,39 @@ TEST(deferredThreadScheduler, test_2_1)
   defaultFun voidFoo = [threadArg] () mutable -> void
   {
     threadArg += "from voidFoo()!!!";
-    std::cout << "[ " << __func__ << " ] "
-              << "voidFoo() running: "
-              << threadArg
-              << "................................."
-              << std::endl;
+    utilities::pclog{} << "[ " << __func__ << " ] "
+                       << "voidFoo() running: "
+                       << threadArg
+                       << "................................."
+                       << std::endl;
   };
 
-  deferredThreadScheduler<defaultFun> dts {"voidFoo", voidFoo, 2s};
+  auto deferredTime = 4s;
+  deferredThreadScheduler<defaultFun> dts {"voidFoo"};
+  dts.registerThread(voidFoo);
 
   ASSERT_EQ(dts.getThreadState(),
-        static_cast<int>(deferredThreadSchedulerBase::threadState::Scheduled));
+            static_cast<int>(deferredThreadSchedulerBase::threadState::NotValid));
+
+  dts.runIn(deferredTime);
+
+  ASSERT_EQ(dts.getThreadState(),
+            static_cast<int>(deferredThreadSchedulerBase::threadState::Scheduled));
+
+  while ( static_cast<int>(deferredThreadSchedulerBase::threadState::Scheduled) == dts.getThreadState() )
+  {
+    usleep(1'000);
+  }
 
   std::cout << "[ " << __func__ << " ] "
-             << "Thread name: "
+            << "Thread name: "
             << dts.getThreadName() << std::endl;
   std::cout << "[ " << __func__ << " ] "
             << "waiting for thread "
             << dts.getThreadName()
-            << " to run..." << std::endl;
+            << " [ id 0x"
+            << std::hex << dts.getThreadId() << std::dec
+            << " ] to run..." << std::endl;
   dts.wait();
 
   ASSERT_EQ(dts.getThreadState(),
@@ -124,25 +167,39 @@ TEST(deferredThreadScheduler, test_2_1)
 void voidFooGlobal();
 void voidFooGlobal()
 {
-  std::cout << "[ " << __func__ << " ] "
-            << "voidFooGlobal() running................................."
-            << std::endl;
+  utilities::pclog{} << "[ " << __func__ << " ] "
+                     << "voidFooGlobal() running................................."
+                     << std::endl;
 };
 
 TEST(deferredThreadScheduler, test_3)
 {
-  deferredThreadScheduler<void()> dts {"voidFooGlobal", voidFooGlobal, 3s};
+  auto deferredTime = 1s;
+  deferredThreadScheduler<void()> dts {"voidFooGlobal"};
+  dts.registerThread(voidFooGlobal);
 
   ASSERT_EQ(dts.getThreadState(),
-          static_cast<int>(deferredThreadSchedulerBase::threadState::Scheduled));
+            static_cast<int>(deferredThreadSchedulerBase::threadState::NotValid));
+
+  dts.runIn(deferredTime);
+
+  ASSERT_EQ(dts.getThreadState(),
+            static_cast<int>(deferredThreadSchedulerBase::threadState::Scheduled));
+
+  while ( static_cast<int>(deferredThreadSchedulerBase::threadState::Scheduled) == dts.getThreadState() )
+  {
+    usleep(1'000);
+  }
 
   std::cout << "[ " << __func__ << " ] "
-             << "Thread name: "
+            << "Thread name: "
             << dts.getThreadName() << std::endl;
   std::cout << "[ " << __func__ << " ] "
             << "waiting for thread "
             << dts.getThreadName()
-            << " to run..." << std::endl;
+            << " [ id 0x"
+            << std::hex << dts.getThreadId() << std::dec
+            << " ] to run..." << std::endl;
   dts.wait();
 
   ASSERT_EQ(dts.getThreadState(),
@@ -151,21 +208,31 @@ TEST(deferredThreadScheduler, test_3)
 
 TEST(deferredThreadScheduler, test_4)
 {
-  deferredThreadScheduler<void()> dts {"voidFooGlobal", voidFooGlobal, 60s};
+  auto deferredTime = 60s;
+  deferredThreadScheduler<void()> dts {"voidFooGlobal"};
+  dts.registerThread(voidFooGlobal);
 
   ASSERT_EQ(dts.getThreadState(),
-          static_cast<int>(deferredThreadSchedulerBase::threadState::Scheduled));
+            static_cast<int>(deferredThreadSchedulerBase::threadState::NotValid));
 
+  dts.runIn(deferredTime);
+
+  ASSERT_EQ(dts.getThreadState(),
+            static_cast<int>(deferredThreadSchedulerBase::threadState::Scheduled));
+
+  usleep(5'000);
   std::cout << "[ " << __func__ << " ] "
             << "Thread name: "
             << dts.getThreadName() << std::endl;
   std::cout << "[ " << __func__ << " ] "
             << "waiting for thread "
             << dts.getThreadName()
-            << " to run..." << std::endl;
+            << " [ id 0x"
+            << std::hex << dts.getThreadId() << std::dec
+            << " ] to run..." << std::endl;
 
-  // cancel the thread after 7 seconds; the test should take at least 7 seconds
-  // and a bit more, but not 60 seconds as requested at the declaration of dts
+  // cancel the thread after 7 seconds; the test should take a bit more than
+  // 7 seconds, not 60 seconds as requested at the declaration of dts
   std::this_thread::sleep_for(7s);
   ASSERT_EQ(dts.getThreadState(),
           static_cast<int>(deferredThreadSchedulerBase::threadState::Scheduled));
@@ -175,6 +242,111 @@ TEST(deferredThreadScheduler, test_4)
   ASSERT_EQ(dts.getThreadState(),
             static_cast<int>(deferredThreadSchedulerBase::threadState::Cancelled));
 }
+
+// schedule 2 threads at the same time and tun them
+TEST(deferredThreadScheduler, test_5)
+{
+  using defaultFun = std::function<void()>;
+  auto deferredTime = 3s;
+  int i{1};
+
+  auto dtsPtr_1 = std::make_shared<deferredThreadScheduler<defaultFun>>("voidFoo");
+  dtsPtr_1.get()->registerThread([i]() -> void
+                    {
+                      utilities::pclog{} << "[ " << __func__ << " ] "
+                                << i
+                                << ": voidFoo() #1 running: "
+                                << "................................."
+                                << std::endl;
+                    });
+  ++i;
+  auto dtsPtr_2 = std::make_shared<deferredThreadScheduler<defaultFun>>("voidFoo");
+  dtsPtr_2.get()->registerThread([i]() -> void
+                    {
+                      utilities::pclog{} << "[ " << __func__ << " ] "
+                                << i
+                                << ": voidFoo() #2 running: "
+                                << "................................."
+                                << std::endl;
+                    });
+
+  dtsPtr_1.get()->runIn(deferredTime);
+  dtsPtr_2.get()->runIn(deferredTime);
+  dtsPtr_1.get()->wait();
+  dtsPtr_2.get()->wait();
+
+  std::cout << "[ " << __func__ << " ] "
+          << "thread 1 "
+          << dtsPtr_1.get()->getThreadName()
+          << " [ id 0x"
+          << std::hex << dtsPtr_1.get()->getThreadId() << std::dec
+          << " ]"
+          << std::endl;
+  std::cout << "[ " << __func__ << " ] "
+          << "thread 2 "
+          << dtsPtr_2.get()->getThreadName()
+          << " [ id 0x"
+          << std::hex << dtsPtr_2.get()->getThreadId() << std::dec
+          << " ]"
+          << std::endl;
+
+  ASSERT_EQ(dtsPtr_1.get()->getThreadState(),
+            static_cast<int>(deferredThreadSchedulerBase::threadState::Run));
+  ASSERT_EQ(dtsPtr_2.get()->getThreadState(),
+            static_cast<int>(deferredThreadSchedulerBase::threadState::Run));
+}
+
+// schedule many threads at the same time and run them
+TEST(deferredThreadScheduler, test_6)
+{
+  using defaultFun = std::function<void()>;
+  using dtsSharedPtr = std::shared_ptr<deferredThreadScheduler<defaultFun>>;
+  std::vector<dtsSharedPtr> v {};
+  auto deferredTime = 3s;
+
+  for(int i = 1; i <= 10; ++i)
+  {
+    //auto dtsPtr = makeSharedDeferredThreadScheduler<defaultFun>("voidFoo");
+    auto dtsPtr = std::make_shared<deferredThreadScheduler<defaultFun>>("voidFoo");
+    dtsPtr.get()->registerThread([i]() -> void
+                    {
+                      utilities::pclog{} << "[ " << __func__ << " ] "
+                                << i
+                                << ": voidFoo() running: "
+                                << "................................."
+                                << std::endl;
+                    });
+    v.push_back(dtsPtr);
+  }
+  for (auto& dts : v)
+  {
+    ASSERT_EQ(dts.get()->getThreadState(),
+              static_cast<int>(deferredThreadSchedulerBase::threadState::NotValid));
+  }
+  for (auto& dts : v)
+  {
+    ASSERT_EQ(dts.get()->getThreadState(),
+              static_cast<int>(deferredThreadSchedulerBase::threadState::NotValid));
+    dts.get()->runIn(deferredTime);
+  }
+  for (auto& dts : v)
+  {
+    ASSERT_EQ(dts.get()->getThreadState(),
+              static_cast<int>(deferredThreadSchedulerBase::threadState::Scheduled));
+  }
+  for (auto& dts : v)
+  {
+    dts.get()->wait();
+  }
+  for (auto& dts : v)
+  {
+    ASSERT_EQ(dts.get()->getThreadState(),
+              static_cast<int>(deferredThreadSchedulerBase::threadState::Run));
+  }
+}
+
+// schedule many threads at the same time and cancel them
+
 ////////////////////////////////////////////////////////////////////////////////
 #pragma clang diagnostic pop
 // END: ignore the warnings when compiled with clang up to here
